25d1c270af1ffd0e846ad4fc604b32e45f4994ba
Nguyen Hoang <hoangtrankhoinguyen@gmail.com>
2019-02-26 22:45:51 +0800

    search movie and starter make the select movie

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const fetch = require("node-fetch");
+const _ = require("lodash");
 
-const app = express();
+const searchMovie = async query => {
+  const url = `https://www.rottentomatoes.com/napi/search/?limit=5&query=${query}`;
+  const response = await fetch(url);
+  const result = await response.json();
+  const formattedResult = _.pick(result, ["movies", "tvSeries"]);
+  console.log(formattedResult);
+  return formattedResult;
+};
 
-app.listen(3000, () => {
-  console.log("Server is running");
-});
+const selectMovie = async movieTitle => {
+  const url = `https://www.rottentomatoes.com/m/${movieTitle}`;
+  const response = await fetch(url);
+  const result = await response.text();
+  console.log(result);
+};
+
+selectMovie("marvels_the_avengers");
